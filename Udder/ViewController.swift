532a69547b45ed03d9e84ccd3144d234013cea02
josemariaaguinigadiaz <A01376669@itesm.mx>
2018-07-12 17:55:40 -0500

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by José María Aguíñiga Díaz on 12/07/18.
+//  Copyright © 2018 José María Aguíñiga Díaz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
